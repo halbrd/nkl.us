9db9d2ac10a6df389d69947fc7aac77497059dcc
Will Holt <halbrd@gmail.com>
2018-03-14 18:18:45 +1100

    convert app to use jpg instead of png to reduce image size (@hugomd)

--- tttmaps/views.py
+++ tttmaps/views.py
@@ -9,16 +9,21 @@ tttmaps = Blueprint('tttmaps', __name__,
 					static_folder='static')
 
 def generate_thumbnail_url(map_name):
-	if os.path.isfile('tttmaps/static/maps/%s_thumbnail.png' % map_name):
-		return url_for('tttmaps.static', filename='maps/%s_thumbnail.png' % map_name)
+	maps_dir = 'tttmaps/static/maps/'
+	if os.path.isfile(maps_dir + '%s_thumbnail.jpg' % map_name):
+		return url_for('tttmaps.static', filename='maps/%s_thumbnail.jpg' % map_name)
 
-	if os.path.isfile('tttmaps/static/maps/cache/%s_thumbnail.png' % map_name):
-		return url_for('tttmaps.static', filename='maps/cache/%s_thumbnail.png' % map_name)
+	cache_dir = maps_dir + 'cache/'
+	if not os.path.exists(cache_dir):
+		os.makedirs(cache_dir)
+
+	if os.path.isfile(cache_dir + '%s_thumbnail.jpg' % map_name):
+		return url_for('tttmaps.static', filename='maps/cache/%s_thumbnail.jpg' % map_name)
 	else:
-		thumbnail = Image.open('tttmaps/static/maps/%s_screenshot_1.png' % map_name)
+		thumbnail = Image.open(maps_dir + '%s_screenshot_1.jpg' % map_name)
 		thumbnail = thumbnail.resize((128, 128), Image.ANTIALIAS)
-		thumbnail.save('tttmaps/static/maps/cache/%s_thumbnail.png' % map_name)
-		return url_for('tttmaps.static', filename='maps/cache/%s_thumbnail.png' % map_name)
+		thumbnail.save(cache_dir + '%s_thumbnail.jpg' % map_name)
+		return url_for('tttmaps.static', filename='maps/cache/%s_thumbnail.jpg' % map_name)
 
 @tttmaps.route('/')
 def index():
@@ -32,18 +37,18 @@ def index():
 
 		screenshots = {}
 		for filename in os.listdir('tttmaps/static/maps'):
-			screenshot_match = re.fullmatch(map_name + '_screenshot_(\d+)\.png', filename)
+			screenshot_match = re.fullmatch(map_name + '_screenshot_(\d+)\.jpg', filename)
 			if screenshot_match:
 				screenshot_index = screenshot_match.group(1)
 				screenshots[screenshot_index] = url_for('tttmaps.static', filename='maps/' + filename)
 		map_['screenshots'] = [ screenshots[screenshot_index] for screenshot_index in sorted(screenshots.keys()) ]
 
-		if os.path.isfile('tttmaps/static/maps/%s_thumbnail.png' % map_name):
-			map_['thumbnail'] = url_for('tttmaps.static', filename='maps/%s_thumbnail.png' % map_name)
+		if os.path.isfile('tttmaps/static/maps/%s_thumbnail.jpg' % map_name):
+			map_['thumbnail'] = url_for('tttmaps.static', filename='maps/%s_thumbnail.jpg' % map_name)
 		elif len(screenshots) > 0:
 			map_['thumbnail'] = generate_thumbnail_url(map_name)
 		else:
-			map_['thumbnail'] = url_for('tttmaps.static', filename='thumbnail_default.png')
+			map_['thumbnail'] = url_for('tttmaps.static', filename='thumbnail_default.jpg')
 
 		data.append(map_)
 
