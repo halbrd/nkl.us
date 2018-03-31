b579b400d0357c3da1bb15dd3792f6dbea61ac41
Will Holt <halbrd@gmail.com>
2018-03-31 20:46:02 +1100

    start tedcruz module

---
+++ tedcruz/views.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, url_for
+
+import requests
+
+tedcruz = Blueprint('tedcruz', __name__,
+					template_folder='templates',
+					static_folder='static')
+
+@tedcruz.route('/')
+def index():
+	#print(requests.get('https://api.twitter.com/1.1/search/tweets.json?q=from%3Acommentiquette%20%40tedcruz&src=typd&lang=en'))
+
+	return render_template('index.html')
