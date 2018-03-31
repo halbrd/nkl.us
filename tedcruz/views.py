a971a8b86087c3ed8e836060b43295ba61166f83
Will Holt <halbrd@gmail.com>
2018-04-01 00:38:12 +1100

    implement search: from:commentiquette @tedcruz (currently broken)

--- tedcruz/views.py
+++ tedcruz/views.py
@@ -1,13 +1,19 @@
 from flask import Blueprint, render_template, url_for
 
 import requests
+import json
+import twitter
 
 tedcruz = Blueprint('tedcruz', __name__,
 					template_folder='templates',
 					static_folder='static')
 
+auth = json.load(open('tedcruz/config.json', 'r'))
+
+api = twitter.Api(consumer_key=auth['consumer_key'], consumer_secret=auth['consumer_secret'], access_token_key=auth['access_token_key'], access_token_secret=auth['access_token_secret'])
+
 @tedcruz.route('/')
 def index():
-	#print(requests.get('https://api.twitter.com/1.1/search/tweets.json?q=from%3Acommentiquette%20%40tedcruz&src=typd&lang=en'))
+	tweets = api.GetSearch(raw_query='l=&q=from%3Acommentiquette%20%40tedcruz&src=typd')
 
-	return render_template('tedcruz.html')
+	return render_template('tedcruz.html', data=tweets)
